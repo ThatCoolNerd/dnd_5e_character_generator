382b4ad6ee53509ded82b3b5035abe3a315155da
ThatCoolNerd <ScottMJfan@gmail.com>
2021-03-20 13:46:09 -0400

    Add files via upload
    Important, intrinsic attributes of the D&D universe

---
+++ dnd_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+races = ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", \
+         "Half-Elf", "Half-Orc", "Tiefling"]
+r_sh = ["dwa", "elf", "halfling", "hum", "dra", "gno", "halfelf", "halforc", \
+        "tiefling"]
+classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", \
+           "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+alig = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", \
+        "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", \
+        "Chaotic Evil"]

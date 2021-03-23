25a329c7c329faa8363149cce7ed3ff0ee6dfd9b
ThatCoolNerd <ScottMJfan@gmail.com>
2021-03-23 19:28:42 -0400

    Update w/ enums, wealth, and names
    Enums used for clarity and legibility.
    
    Some wealth generation brought here from dnd_char.
    
    Fixed where the last name in a list would print a newline.

--- ran_gen.py
+++ ran_gen.py
@@ -1,61 +1,74 @@
 #!/usr/bin/python
 
-##imports
+# imports
+from dnd_world import World
 import random
 import die
+import re
 
-races = ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", \
-         "Half-Elf", "Half-Orc", "Tiefling"]
-r_sh = ["dwa", "elf", "halfling", "hum", "dra", "gno", "halfelf", "halforc", \
-        "tiefling"]
+# races = World.RACES.value
+# r_sh = World.R_SH.value
+# classes = World.CLASSES.value
+# alig = World.ALIG.value
 
-##functions
-def rfname(r) :
+
+# functions
+def rfname(r):
     "Pull a random first name from a text file based on race"
     
-    for i in range(len(races)) :
-        if r == races[i] :
-            path = f"names/{r_sh[i]}_fnames.txt"
+    for i in range(len(World.RACES.value)):
+        if r == World.RACES.value[i]:
+            path = f"names/{World.R_SH.value[i]}_fnames.txt"
             name = open(path, "r")
             break
     
-    names = name.read().split(', ')
+    names = name.read().split(';')
     name.close()
-    return random.choice(names)
+    return re.sub("\n", "", random.choice(names))
 
-def rlname(r) :
+def rlname(r):
     "Pull a random last name from a text file based on race"
     
-    for i in range(len(races)) :
-        if r == races[i] :
-            path = f"names/{r_sh[i]}_lnames.txt"
+    for i in range(len(World.RACES.value)):
+        if r == World.RACES.value[i]:
+            path = f"names/{World.R_SH.value[i]}_lnames.txt"
             name = open(path, "r")
             break
     
-    names = name.read().split(', ')
+    names = name.read().split(';')
     name.close()
-    return random.choice(names)
+    return re.sub("\n", "", random.choice(names))
     
-def rrace() :
+def rrace():
     "Return random race"
-    return random.choice(races)
+    return random.choice(World.RACES.value)
 
-def rclass() : 
+def rclass(): 
     "Return random class"
+    return random.choice(World.CLASSES.value)
     
-    classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", \
-               "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
-    return random.choice(classes)
-    
-def ralignment() : 
+def ralignment(): 
     "Return random alignment"
-    
-    alig = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", \
-            "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", \
-            "Chaotic Evil"]
-    return random.choices(alig)
+    return random.choices(World.ALIG.value)
 
-def rwealth() :
+def rwealth():
     "Return a random amount of wealth (in GP)"
-    
     return int(die.rolld(15000) * (random.randrange(82, 99) / 100))
+
+def get_wealth_desc(w): 
+        "Return a random description of the character's appearance (GP based)"
+        
+        if w <= 100:
+            txt = open("wealth_descs/s_poor.txt", "r")
+        elif w <= 1250:
+            txt = open("wealth_descs/poor.txt", "r")
+        elif w <= 5800:
+            txt = open("wealth_descs/moderate.txt", "r")
+        elif w <= 12500:
+            txt = open("wealth_descs/wealthy.txt", "r")
+        elif w >= 12500:
+            txt = open("wealth_descs/s_wealthy.txt", "r")
+            
+        pot_descs = txt.read().split(",   ")
+        txt.close()
+        return re.sub("\n", "", random.choice(pot_descs))

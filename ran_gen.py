8f846edb846e1c40e12b5319870c064fe45bee1f
ThatCoolNerd <ScottMJfan@gmail.com>
2021-04-14 17:37:24 -0400

    Added weapons and armor
    print_weapons() was a placeholder function and will almost definitely be moved somewhere out of ran_gen.py

--- ran_gen.py
+++ ran_gen.py
@@ -20,24 +20,33 @@ def rname(r, pos):
     
     names = name.read().split(';')
     name.close()
+    #for kind, choices in World.CL_GEAR_STER.value.items():
+    #    print(f"{kind}: {choices}")
     return re.sub("\n", "", random.choice(names))
 
-def rrace():
-    "Return random race"
-    return random.choice(World.RACES.value)
-
-def rclass(): 
-    "Return random class"
-    return random.choice(World.CLASSES.value)
-    
-def ralignment(): 
-    "Return random alignment"
-    return random.choices(World.ALIG.value)
+def rrace(): return random.choice(World.RACES.value)
+def rclass(): return random.choice(World.CLASSES.value)
+def ralignment(): return random.choices(World.ALIG.value)
 
 def rwealth():
     "Return a random amount of wealth (in GP)"
     return int(die.rolld(World.W_THRESH.value[-1]) * \
         (random.randrange(82, 99) / 100))
+
+def rclothing():
+    "Return a randomly generated article of outside clothing"
+    armor_type = random.choice(list(World.ARMOR.value.keys()))
+    armor = random.choice(World.ARMOR.value[armor_type]["ar_kind"])
+    return armor
+
+def rweapon():
+    kind = random.choice(list(World.WEAPON.value.keys()))
+    weapon = random.choice(list(World.WEAPON.value[kind].keys()))
+    
+    if weapon == "Dart":
+        return "handful of darts"
+    else:
+        return weapon
 
 def get_wealth_desc(w): 
     "Return a random description of the character's appearance (GP based)"
@@ -48,6 +57,29 @@ def get_wealth_desc(w):
             txt = open(f"wealth_descs/{wd_txt_name[i]}.txt", "r")
             break
         
+    if w > World.W_THRESH.value[-1]:
+        txt = open(f"wealth_descs/{wd_txt_name[-1]}.txt", "r")
+    
     pot_descs = txt.read().split(";")
     txt.close()
     return re.sub("\n", "", random.choice(pot_descs))
+
+def print_weapons():
+    for typee, listt in World.WEAPON.value.items():
+        print(f"{typee}:\n")
+        for weapon, price in listt.items():
+            if price < .1:
+                cost = int(price * 100)
+                currency = "CP"
+            elif price < 1:
+                cost = int(price * 10)
+                currency = "SP"
+            else:
+                cost = price
+                currency = "GP"
+            
+            print("   ", end = "")
+            print(weapon.ljust(18, "."), end = "")
+            print("| {:>2} ".format(cost), end = "")
+            print(currency)
+        print()
